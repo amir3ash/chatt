5fd32d990d29cdffced8468bf98606b9c0b16998
Amir Shams <amir3.ash@gmail.com>
2024-09-03 17:25:10 +0330

    feat: move otel utils to pkg/observe

--- cmd/api-server/main.go
+++ cmd/api-server/main.go
@@ -7,8 +7,8 @@ import (
 	"chat-system/core/messages"
 	"chat-system/core/repo"
 	kafkarep "chat-system/core/repo/kafkaRep"
+	"chat-system/pkg/observe"
 	"context"
-	"errors"
 	"fmt"
 	"time"
 
@@ -17,135 +17,7 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.opentelemetry.io/contrib/instrumentation/go.mongodb.org/mongo-driver/mongo/otelmongo"
-	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
-	"go.opentelemetry.io/otel/exporters/stdout/stdoutlog"
-	"go.opentelemetry.io/otel/exporters/stdout/stdoutmetric"
-
-	// "go.opentelemetry.io/otel/log/global"
-	"go.opentelemetry.io/otel/propagation"
-	"go.opentelemetry.io/otel/sdk/log"
-	"go.opentelemetry.io/otel/sdk/metric"
-	"go.opentelemetry.io/otel/sdk/resource"
-	"go.opentelemetry.io/otel/sdk/trace"
-	semconv "go.opentelemetry.io/otel/semconv/v1.24.0"
 )
-
-// setupOTelSDK bootstraps the OpenTelemetry pipeline.
-// If it does not return an error, make sure to call shutdown for proper cleanup.
-func setupOTelSDK(ctx context.Context) (shutdown func(context.Context) error, err error) {
-	var shutdownFuncs []func(context.Context) error
-
-	// shutdown calls cleanup functions registered via shutdownFuncs.
-	// The errors from the calls are joined.
-	// Each registered cleanup will be invoked once.
-	shutdown = func(ctx context.Context) error {
-		var err error
-		for _, fn := range shutdownFuncs {
-			err = errors.Join(err, fn(ctx))
-		}
-		shutdownFuncs = nil
-		return err
-	}
-
-	// handleErr calls shutdown for cleanup and makes sure that all errors are returned.
-	handleErr := func(inErr error) {
-		err = errors.Join(inErr, shutdown(ctx))
-	}
-
-	// Set up propagator.
-	prop := newPropagator()
-	otel.SetTextMapPropagator(prop)
-
-	// Set up trace provider.
-	tracerProvider, err := newTraceProvider()
-	if err != nil {
-		handleErr(err)
-		return
-	}
-	shutdownFuncs = append(shutdownFuncs, tracerProvider.Shutdown)
-	otel.SetTracerProvider(tracerProvider)
-
-	// Set up meter provider.
-	// meterProvider, err := newMeterProvider()
-	// if err != nil {
-	// 	handleErr(err)
-	// 	return
-	// }
-	// shutdownFuncs = append(shutdownFuncs, meterProvider.Shutdown)
-	// otel.SetMeterProvider(meterProvider)
-
-	// // Set up logger provider.
-	// loggerProvider, err := newLoggerProvider()
-	// if err != nil {
-	// 	handleErr(err)
-	// 	return
-	// }
-	// shutdownFuncs = append(shutdownFuncs, loggerProvider.Shutdown)
-	// global.SetLoggerProvider(loggerProvider)
-
-	return
-}
-
-func newPropagator() propagation.TextMapPropagator {
-	// return propagation.NewCompositeTextMapPropagator(
-	// 	propagation.TraceContext{},
-	// 	propagation.Baggage{},
-	// )
-	return propagation.TraceContext{}
-}
-
-func newTraceProvider() (*trace.TracerProvider, error) {
-	ctx := context.Background()
-	res := resource.NewWithAttributes(
-		semconv.SchemaURL,
-		semconv.ServiceName("chat-api"),
-	)
-
-	traceExporter, err := otlptracehttp.New(ctx,
-		otlptracehttp.WithEndpoint("jaeger-collector.default.svc.cluster.local:4318"),
-		otlptracehttp.WithInsecure(),
-		otlptracehttp.WithCompression(otlptracehttp.GzipCompression),
-	)
-	if err != nil {
-		return nil, err
-	}
-
-	bsp := trace.NewBatchSpanProcessor(traceExporter, trace.WithBlocking())
-
-	traceProvider := trace.NewTracerProvider(
-		trace.WithSampler(trace.AlwaysSample()),
-		trace.WithSpanProcessor(bsp),
-		trace.WithResource(res),
-	)
-	return traceProvider, nil
-}
-
-func newMeterProvider() (*metric.MeterProvider, error) {
-	metricExporter, err := stdoutmetric.New()
-	if err != nil {
-		return nil, err
-	}
-
-	meterProvider := metric.NewMeterProvider(
-		metric.WithReader(metric.NewPeriodicReader(metricExporter,
-			// Default is 1m. Set to 3s for demonstrative purposes.
-			metric.WithInterval(3*time.Second))),
-	)
-	return meterProvider, nil
-}
-
-func newLoggerProvider() (*log.LoggerProvider, error) {
-	logExporter, err := stdoutlog.New()
-	if err != nil {
-		return nil, err
-	}
-
-	loggerProvider := log.NewLoggerProvider(
-		log.WithProcessor(log.NewBatchProcessor(logExporter)),
-	)
-	return loggerProvider, nil
-}
 
 func getMessageRepository(conf *config.Confing) messages.Repository {
 	type messageRepoType int
@@ -181,13 +53,13 @@ func getMessageRepository(conf *config.Confing) messages.Repository {
 
 	case kafkaRepoT:
 		kafkaWriter := &kafka.Writer{
-			Addr:                   kafka.TCP("kafka-controller-headless.default.svc.cluster.local"),
+			Addr:                   kafka.TCP(conf.KafkaHost),
 			Topic:                  "chat-messages",
 			AllowAutoTopicCreation: true,
 			// Balancer:               &kafka.LeastBytes{},
-			RequiredAcks:           kafka.RequireOne,
-			BatchTimeout:           50 * time.Millisecond,
-			Compression:            kafka.Snappy,
+			RequiredAcks: kafka.RequireOne,
+			BatchTimeout: 50 * time.Millisecond,
+			Compression:  kafka.Snappy,
 
 			// Transport: kafka.DefaultTransport,
 		}
@@ -206,7 +78,7 @@ func main() {
 		panic(err)
 	}
 
-	otelShutdown, err := setupOTelSDK(context.TODO())
+	otelShutdown, err := observe.SetupOTelSDK(context.TODO())
 	if err != nil {
 		panic(fmt.Errorf("can't setup opentelementry: %w", err))
 	}
